f8b125514a9345855744b142d7da2be77810c64c
angelacaterina <angela.caterina@icloud.com>
2021-10-28 12:11:58 +0200

    Add Loop
    modificata la struttura App creando una nuova funzione che crea un nuovo componente della Card, per poi poterlo mappare, {contacts.map(createCard)}

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,25 @@ import Avatar from "./Avatar";
 import Card from "./Card";
 import contacts from "../contacts";
 
+// funzione per creare un nuovo componente della Card
+function createCard(contact){
+  return <Card
+    key={contact.id}
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}
+  />
+}
+// key: deve essere univoco per ogni componente mappato, per questo gli viene asseganto il valore dell' id
+
 function App() {
-  console.log(contacts)
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQGEGjbJULQ_BA/profile-displayphoto-shrink_200_200/0/1618306439081?e=1639612800&v=beta&t=FGWuj5EkfBCuG1MHnmO-3yV0HIqsilX8oV6WMMgdrzk"/>
-      <Card 
+      {/* SENZA MAPPATURA */}
+      {/* <Card 
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -21,13 +33,15 @@ function App() {
         tel={contacts[1].phone}
         email={contacts[1].email}
       />
-
       <Card 
         name={contacts[2].name}
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
+
+      {/* MAPPATURA DEI COMPONENTI, LOOP */}
+      {contacts.map(createCard)}
     </div>
   );
 }
